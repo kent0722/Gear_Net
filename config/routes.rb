464c372a3a80498fe159c518bc5b934675ffaaad
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-08-16 16:05:19 +0900

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   post 'guest_login', to: 'user_sessions#guest_login'
   get '/login', to:'user_sessions#new'
-  
   post '/login', to:'user_sessions#create'
   delete '/logout', to:'user_sessions#destroy'
   root 'home#top'
   get 'pages/Terms_of_use'
   get 'pages/policy'
-  resources :users, only: %i[:new :create]
+  resources :users, only: %i[new create]
   resources :posts do
     resources :comments, only: %i[create destroy], shallow: true
     resource :likes, only: %i[create destroy]
