fb0f765b79a938d36eeeec4f4709209b8f578ff7
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-08-22 01:14:43 +0900

    いいねの機能のAjax化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,13 @@ class LikesController < ApplicationController
   before_action :guest_authenticated, only: %i[create destroy] 
 
   def create
+    @post = Post.find(params[:post_id])
     @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_to request.referer
   end
 
   def destroy 
     @post = Post.find(params[:post_id])
     @like = current_user.likes.find_by(post_id: @post.id)
-    @like.destroy
-    redirect_to request.referer
+    @like.destroy!
   end
 end
