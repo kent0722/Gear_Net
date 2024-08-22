8d4fad4d3a43dcb082cbe8815d1ef5471cce0ab8
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-08-22 14:13:10 +0900

    ログイン機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   def own?(object)
     id == object&.user_id
