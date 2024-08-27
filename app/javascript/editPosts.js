4301ea76a1299d12df5df7562b9c64cb613f259f
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-08-27 16:22:31 +0900

    images画像フォーム完成

--- app/javascript/editPosts.js
+++ app/javascript/editPosts.js
@@ -3,17 +3,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const openModal = document.getElementById("openModal");
     const closeModal = document.getElementById("closeModal");
     const modal = document.getElementById("modal");
+
     const openSmModal = document.getElementById("openSmModal");
     const closeSmModal = document.getElementById("closeSmModal");
     const smModal = document.getElementById("smModal");
+
     const editBrandInput = document.getElementById('edit_brand');
     const nodeList = document.querySelectorAll('#edit_brand-list option')
     const editDatalistOptions = Array.from(nodeList).map(option => option.value);
     const editCategoryInput = document.getElementById('edit_category');
     const editInstrumentModelInput = document.getElementById('edit_instrument_model')
-    const editFileInput = document.getElementById('editFile-input');
-    const editFileSvg = document.getElementById('editFileSvg')
-    const editImagePreview = document.getElementById('editImage-preview');
+    // image_upload
+    const editFileInput = document.getElementById('edit-file-input');
+    const editFileSvg = document.getElementById('edit-fileSvg');
+    const editImagePreview = document.getElementById('edit-image-preview');
+    // preview
+    const editPostForm = document.getElementById('edit_post');
+    const editPreviewList = document.getElementById('edit-previews');
+    if (!editPostForm) return;
 
     if(openModal){
       //クリックでモーダルを開く
@@ -34,69 +41,101 @@ document.addEventListener('DOMContentLoaded', () => {
           document.body.style.overflow = "";
         }
       });
+    }
 
-      if(openSmModal){
-        openSmModal.addEventListener('click', () => {
-          smModal.style.display = 'flex';
-          document.body.style.overflow = 'hidden';
-        })
-        closeSmModal.addEventListener('click', () => {
-          smModal.style.display = "none";
+    if(openSmModal){
+      openSmModal.addEventListener('click', () => {
+        smModal.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+      })
+      closeSmModal.addEventListener('click', () => {
+        smModal.style.display = "none";
+        document.body.style.overflow = "";
+      })
+      smModal.addEventListener('click', (e) => {
+        if (e.target === smModal) {
+          smModal.style.display = 'none';
           document.body.style.overflow = "";
-        })
-        smModal.addEventListener('click', (e) => {
-          if (e.target === smModal) {
-            smModal.style.display = 'none';
-            document.body.style.overflow = "";
-          }
-        });
-      }
+        }
+      });
+    }
 
-       //brand 入力制限
-      if(editBrandInput){
-        editBrandInput.addEventListener('input', () => {
-          const brandValue = editBrandInput.value;
-          if (!editDatalistOptions.includes(brandValue)) {
-            editBrandInput.value = '';
+    //brand 入力制限
+    if(editBrandInput){
+      editBrandInput.addEventListener('input', () => {
+        const brandValue = editBrandInput.value;
+        if (!editDatalistOptions.includes(brandValue)) {
+          editBrandInput.value = '';
+        }
+      });
+    }
+    //category 入力補助
+    if (editCategoryInput) {
+      editCategoryInput.addEventListener('focus', () => { 
+        editCategoryInput.setAttribute('placeholder', "例: Telecaster, Les paul, AZ(Ibanez)");
+      });
+      editCategoryInput.addEventListener('blur', () => {
+        editCategoryInput.setAttribute('placeholder', ' ');
+      });
+    }
+    //instrument_model 入力補助
+    if (editInstrumentModelInput) {
+    editInstrumentModelInput.addEventListener('focus', () => {
+        editInstrumentModelInput.setAttribute('placeholder', '例: Vintage Model')
+      })
+      editInstrumentModelInput.addEventListener('blur', () => {
+        editInstrumentModelInput.setAttribute('placeholder', '')
+      })
+    }
+
+    // PostEdit image_upload
+    if(editFileInput){
+      editFileInput.addEventListener('change', (e) => {
+        const files = Array.from(e.target.files);
+
+        if(files.length > 6) {
+          alert("投稿できる画像は6枚までです");
+          e.target.value = ''; // ファイル選択をリセット
+          return;
+        }
+
+        const file = files[0]
+        if (file) {
+          const editReader = new FileReader();
+          editReader.readAsDataURL(file);
+          editReader.onload = (e) => {
+            editImagePreview.src = e.target.result;
+            editImagePreview.classList.remove('hidden');
+            editFileSvg.classList.add('hidden');
           }
-        });
-      }
-      //category 入力補助
-      if (editCategoryInput) {
-        editCategoryInput.addEventListener('focus', () => { 
-          editCategoryInput.setAttribute('placeholder', "例: Telecaster, Les paul, AZ(Ibanez)");
-        });
-        editCategoryInput.addEventListener('blur', () => {
-          editCategoryInput.setAttribute('placeholder', ' ');
-        });
-      }
-      //instrument_model 入力補助
-      if (editInstrumentModelInput) {
-      editInstrumentModelInput.addEventListener('focus', () => {
-          editInstrumentModelInput.setAttribute('placeholder', '例: Vintage Model')
-        })
-        editInstrumentModelInput.addEventListener('blur', () => {
-         editInstrumentModelInput.setAttribute('placeholder', '')
-        })
-      }
-      //PostEdit imageアップロード
-      if(editFileInput){
-        editFileInput.addEventListener('change', (e) => {
-          const file = e.target.files[0];
-          if (file) {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = (e) => {
-              editImagePreview.src = e.target.result;
-              editImagePreview.classList.remove('hidden');
-              editFileSvg.classList.add('hidden')
-            }
-          } else {
-            editImagePreview.classList.add('hidden');
-            editFileSvg.classList.remove('hidden') 
-          }  
-        });
-      }
+        } else {
+          editImagePreview.classList.add('hidden');
+          editFileSvg.classList.remove('hidden');
+        }
+        
+        // preview
+        editPreviewList.innerHTML = '';
+        files.map((file) => {
+          const blob = URL.createObjectURL(file);
+          const previewWrapper = document.createElement('div');
+          previewWrapper.setAttribute('class', 'preview flex-shrink-0 mx-1');
+
+          const previewImage = document.createElement('img');
+          previewImage.setAttribute('class', 'preview-image');
+          previewImage.setAttribute('src', blob);
+          previewImage.style.width = '70px';
+          previewImage.style.height = '100px';
+          previewImage.style.objectFit = 'cover';
+
+          // 画像クリック時に label の imagePreview に表示
+          previewImage.addEventListener('click', () => {
+            editImagePreview.src = blob;
+          });
+
+          previewWrapper.appendChild(previewImage);
+          editPreviewList.appendChild(previewWrapper);
+        });     
+      });
     }
   }
   document.addEventListener("turbo:load", editModal);
