cf8b01d3091667c174afef5b979dfc89407cf8ae
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-05 20:54:15 +0900

    follow機能 Ajax化完了

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,14 @@
 class RelationshipsController < ApplicationController
+  before_action :require_login
+  before_action :guest_authenticated, only: %i[create destroy] 
+  
   def create
+    @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    redirect_to request.referer
   end
   
   def destroy
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
   end
 end
