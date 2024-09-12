d7c0071e717d69d46736d7f1d918d4cc50e82759
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-12 15:36:24 +0900

    production.rbの修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,6 @@ class UserMailer < ApplicationMailer
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email, 
-      subject: 'パスワード再発行のご案内',
-      content_type: "text/html")
+      subject: 'パスワード再発行のご案内')
   end
 end
