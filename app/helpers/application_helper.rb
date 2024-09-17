a30ccf9619c25ac413e2654744de7344aeab294a
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-18 00:45:18 +0900

    OGP設定の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,13 +25,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('haikei3.jpg'),
+        image: image_url('defaultUser.jpg'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@',
-        image: image_url('haikei3.jpg')
+        image: image_url('defaultUser.jpg')
       }
     }
   end
