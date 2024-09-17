e3e5cfcf0a98c0f4b7a80e85e0ae769fdd2b656a
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-18 00:19:09 +0900

    OGP設定の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,34 +8,4 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
-
-  def default_meta_tags
-    {
-      site: 'GearNET.',
-      title: 'GearNET.',
-      reverse: true,
-      charset: 'utf-8',
-      description: 'GearNET.は、ギターやベースなどの楽器を愛する人々のためのコミュニティーツールです。楽器の紹介、情報交換、仲間作りができます。',
-      keywords: '',
-      canonical: request.original_url,
-      separator: '|',
-      icon: [
-        { href: image_url('favicon.ico') },
-        { href: image_url('haikei3.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-      ],
-      og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
-        type: 'website',
-        url: request.original_url,
-        image: image_url('haikei3.jpg'),# 配置するパスやファイル名によって変更する
-        local: 'ja-JP',
-      },
-      twitter: {
-        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
-        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
-      }
-    }
-  end
 end
