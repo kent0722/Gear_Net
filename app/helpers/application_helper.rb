af8b8b15487f7cfb53f8527a1c0f97bb45365c28
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-18 00:02:09 +0900

    OGP設定の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,12 +16,12 @@ module ApplicationHelper
       reverse: true,
       charset: 'utf-8',
       description: 'GearNET.は、ギターやベースなどの楽器を愛する人々のためのコミュニティーツールです。楽器の紹介、情報交換、仲間作りができます。',
-      keywords: '楽器,コミュニティ,音楽',
+      keywords: '',
       canonical: request.original_url,
       separator: '|',
       icon: [
-        { href: image_url("favicon.ico"), sizes: "60x60" },
-        { href: image_url('haikei3.jpg'), rel: 'apple-touch-icon', sizes: "90x90", type: 'image/jpg' },
+        { href: image_url('favicon.ico') },
+        { href: image_url('haikei3.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       ],
       og: {
         site_name: :site,
@@ -29,13 +29,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('haikei3.jpg'),
+        image: image_url('haikei3.jpg'),# 配置するパスやファイル名によって変更する
         local: 'ja-JP',
       },
       twitter: {
-        card: 'summary_large_image',
-        site: '@',
-        image: image_url('haikei3.jpg'),
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
+        site: '@', # アプリの公式Twitterアカウントがあれば、アカウント名を記載
+        image: image_url('haikei3.jpg'),# 配置するパスやファイル名によって変更
       }
     }
   end
