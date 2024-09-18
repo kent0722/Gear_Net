117069b0d6d13f83a09a877ab86ba868cbdbb792
kent0722 <moon1_1light0722@yahoo.co.jp>
2024-09-19 00:31:13 +0900

    message機能実装開始

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,7 @@
+class RoomsController < ApplicationController
+  def create; end
+
+  def index; end
+  
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     end
     resource :profile, only: %i[show edit update destroy]
     resource :relationships, only: %i[create destroy]
+    resources :messages, only: [:create]
+    resources :rooms, only: [:create, :index, :show]
     resources :notifications, only: %i[index] do
       collection do
         delete :delete_selected
